775d9c2e6624b55de8290cee1c1b39904d2644bf
nkyono <nkyono97@gmail.com>
2020-08-14 19:02:33 -0700

    added map centering on user's geolocation (if they allow it) or on center of united states by default)

--- src/components/add/mapInput.js
+++ src/components/add/mapInput.js
@@ -2,6 +2,31 @@ import {Map, GoogleApiWrapper} from 'google-maps-react';
 import React from 'react';
 
 class MapInput extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      lat:39.8283,
+      lng:-98.5795
+    }
+  }
+
+  setUserPosition = (position) => {
+    this.setState({
+      lat:position.coords.latitude,
+      lng:position.coords.longitude
+    })
+  }
+
+  setUserPositionErr = () => {
+    this.setState({
+      lat:39.8283,
+      lng:-98.5795
+    })
+  }
+
+  componentDidMount(){
+    navigator.geolocation.getCurrentPosition(this.setUserPosition, this.setUserPositionErr)
+  }
 
   retLatLng = (t, map, coord) => {
     const {latLng} = coord
@@ -13,10 +38,10 @@ class MapInput extends React.Component {
       <div>
         <Map
             google={this.props.google}
-            zoom={5}
-            initialCenter={{
-              lat:39.8283,
-              lng:-98.5795
+            zoom={4}
+            center={{
+              lat:this.state.lat,
+              lng:this.state.lng
             }}
             onClick = {this.retLatLng}
         >
