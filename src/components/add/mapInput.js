08f91b0a06bcddc8d82142c919a939d617b1d049
nkyono <nkyono97@gmail.com>
2020-08-16 22:50:18 -0700

    changed how the map is centered on initial load. Fixed bug where map would re-center itself on click.

--- src/components/add/mapInput.js
+++ src/components/add/mapInput.js
@@ -5,28 +5,58 @@ class MapInput extends React.Component {
   constructor(props){
     super(props)
     this.state = {
-      lat:39.8283,
-      lng:-98.5795
+      lat: 39.8283,
+      lng: -98.5795,
+      isLoaded: false,
     }
+  }
+
+  setUserPosition = (position) => {
+    this.setState({
+      lat:position.coords.latitude,
+      lng:position.coords.longitude,
+      isLoaded: true,
+    })
+  }
+
+  setUserPositionErr = () => {
+    this.setState({
+      isLoaded: true,
+    })
+  }
+
+  componentDidMount(){
+    navigator.geolocation.getCurrentPosition(this.setUserPosition, this.setUserPositionErr)
   }
 
   retLatLng = (t, map, coord) => {
     const {latLng} = coord
+    this.setState({
+      lat: latLng.lat(),
+      lng: latLng.lng()
+    })
     this.props.mapCoordCallback(latLng.lat(), latLng.lng())
   }
 
   render() {
-    return  (
-      <div>
-        <Map
-            google={this.props.google}
-            zoom={4}
-            centerAroundCurrentLocation="true"
-            onClick = {this.retLatLng}
-        >
-        </Map>
-      </div>
-    );
+    if (!this.state.isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return  (
+        <div>
+          <Map 
+              google={this.props.google}
+              zoom={4}
+              initialCenter={{
+                lat: this.state.lat,
+                lng: this.state.lng
+              }}
+              onClick = {this.retLatLng}
+          >
+          </Map>
+        </div>
+      );
+    }
   }
 }
   
